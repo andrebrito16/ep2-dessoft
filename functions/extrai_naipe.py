bd15a0a052e2bdb4190b770e5d8cbea6453baa3c
andrebrito16 <felipe.brito016@gmail.com>
2021-04-19 19:12:56 -0400

    função extrai_naipe atualizada

--- functions/extrai_naipe.py
+++ functions/extrai_naipe.py
@@ -1,5 +1,4 @@
-def extrai_naipe(cartas):
-    if len(cartas) == 3:
-        return cartas[2]
-    else:
-        return cartas[1]
+def extrai_naipe(carta):
+    return str(carta)[::-1][0]
+
+print(extrai_naipe("12♦"))
