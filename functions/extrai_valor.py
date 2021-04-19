659bb183985b11bb893b889576f328008740cf4c
andrebrito16 <felipe.brito016@gmail.com>
2021-04-19 19:15:11 -0400

    função extrai_valor atualizada

--- functions/extrai_valor.py
+++ functions/extrai_valor.py
@@ -1,5 +1,4 @@
 def extrai_valor(cartas):
-    if len(cartas) == 3:
-        return cartas[0]+cartas[1]
-    else:
-        return cartas [0]
+    return str(cartas)[:-1]
+
+print(extrai_valor("A♣"))
