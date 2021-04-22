8ea5abbf3aef757f7b8aec0830db66b10bf66688
FIRST_NAME LAST_NAME <felipe.brito016@gmail.com>
2021-04-22 14:10:02 -0400

    versão 1 do jogo finalizada

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 
 # Importações das funções
-from functions.cria_baralho import cria_baralho
-from functions.empilha import empilha
-from functions.lista_movimentos_possiveis import lista_movimentos_possiveis
-from functions.possui_movimentos_possiveis import possui_movimentos_possiveis
+from funcoes import *
 
 
 print('''
@@ -24,5 +21,65 @@ Para que um movimento possa ser realizado basta que uma das duas condições aba
 
 Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada. 
 
-Aperte [Enter] para iniciar o jogo...
 ''')
+# input("Aperte [Enter] para iniciar o jogo...")
+
+
+baralho = cria_baralho()
+
+while True:
+    if len(baralho) == 0:
+        print("Você GANHOU! ")
+        break
+    indice = 1
+    for carta in baralho:
+        print(f"{indice}. {carta}")
+        indice += 1
+    
+    if possui_movimentos_possiveis(baralho):
+        movimento = int(input("Escolha uma carta para movimentar: "))
+        possibilidades = lista_movimentos_possiveis(baralho, movimento-1)
+        if len(possibilidades) == 0:
+            print("Essa carta não pode ser movida, tente novamente!")
+        else:
+            if len(possibilidades) > 1:
+                print(f"Essas são as opções para movimento da carta {baralho[movimento-1]}")
+                indice_possibilidades = 1
+                for p in possibilidades:
+                    print(f"{indice_possibilidades}. {baralho[movimento-1-p]}")
+                    indice_possibilidades += 1
+                while True:
+                    escolha_movimento = int(input("Escolha uma opção: ").strip())
+                    if escolha_movimento > len(possibilidades) and escolha_movimento < 1:
+                        print("Escolha inválida, tente novamente!")
+                        indice_possibilidades = 1
+                        for p in possibilidades:
+                            print(f"{indice_possibilidades}. {baralho[movimento-p]}")
+                            indice_possibilidades += 1
+                    else:
+                        print(f"Empilhar a carta {baralho[movimento-1]} sobre a carta {baralho[movimento-1-possibilidades[escolha_movimento-1]]}")
+                        baralho[movimento-1-possibilidades[escolha_movimento-1]] = baralho[movimento-1] 
+                        del baralho[movimento-1]
+                        break
+            elif len(possibilidades) == 0:
+                print("Nenhum movimento possível, tente novamente!")
+            else:
+                # Remove a carta realizando o único movimento possível!
+
+                baralho[movimento-1-possibilidades[0]] = baralho[movimento-1] 
+                del baralho[movimento-1]
+    else:
+        print("Você perdeu!")
+                
+                
+        
+            
+            
+
+            
+                   
+           
+            
+            
+    
+        
---
+++ funcoes.py
@@ -0,0 +1,58 @@
+from random import shuffle
+
+def cria_baralho():
+    lista =  ['10♠', '10♥', '10♦', '10♣','9♠', '9♥', '9♦', '9♣', '8♠', '8♥', '8♦','8♣', '7♠', '7♥', '7♦', '7♣', '6♠', '6♥','6♦', '6♣', '5♠', '5♥', '5♦', '5♣', '4♠','4♥', '4♦', '4♣', '3♠', '3♥', '3♦', '3♣','2♠', '2♥', '2♦', '2♣', 'A♠', 'A♥', 'A♦','A♣', 'K♠', 'K♥', 'K♦', 'K♣', 'J♠', 'J♥','J♦', 'J♣', 'Q♠', 'Q♥', 'Q♦', 'Q♣']
+    shuffle(lista)
+    return lista
+
+def empilha(baralho, origem, destino):
+
+    baralho[destino] = baralho[origem]
+
+    del baralho[origem]
+    
+    return baralho
+
+
+
+def extrai_naipe(carta):
+    return str(carta)[::-1][0]
+
+
+def extrai_valor(cartas):
+    return str(cartas)[:-1]
+
+
+def lista_movimentos_possiveis(baralho, posicao):
+    if posicao == 0:
+        return []
+
+    movimentos_possiveis = list()
+
+    valor = extrai_valor(baralho[posicao])
+    naipe = extrai_naipe(baralho[posicao])
+
+    if posicao < 3:
+        if valor == extrai_valor(baralho[posicao-1]) or naipe == extrai_naipe(baralho[posicao-1]):
+            movimentos_possiveis.append(1)
+        return movimentos_possiveis
+
+
+    if valor == extrai_valor(baralho[posicao-1]) or naipe == extrai_naipe(baralho[posicao-1]):
+        movimentos_possiveis.append(1)
+    if valor == extrai_valor(baralho[posicao-3]) or naipe == extrai_naipe(baralho[posicao-3]):
+        movimentos_possiveis.append(3)
+    
+    return movimentos_possiveis
+
+def possui_movimentos_possiveis(baralho):
+    possivel = False
+
+    for i in range(0, len(baralho)):
+        if len(lista_movimentos_possiveis(baralho, i)) > 0:
+            possivel = True
+    
+    return possivel
+
+
+
