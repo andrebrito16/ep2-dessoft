c3cf2d11ab58ae739b275bd5440cc50ee576d44e
andrebrito16 <felipe.brito016@gmail.com>
2021-05-05 15:43:24 -0400

    added readme.md and exception on exit

---
+++ README.md
@@ -0,0 +1,23 @@
+# EP2 - Dessoft
+
+##### Projeto desenvolvido no primeiro semestre do ano de 2021 no curso de Engenharia do Insper
+---
+## Autores
+- Gabriela Albuquerque - Turma 1A
+- André Brito - Turma 1A
+---
+## Descrição do programa
+O jogo *solitaire accordion* foi implementado usando Python. Nenhuma interface gráfica foi construída, portanto o jogo deve ser jogado pelo próprio terminal.
+
+**AVISO:** Na exibição de caracteres no terminal foram usadas diferentes cores, que podem variar de acordo com a configuração de seu terminal, portanto caso não visualize alguma parte tente alterar a cor das letras e/ou fundo do terminal.
+
+---
+## Preview
+Abaixo temos um exemplo do jogo.
+<img src="Animação.gif" width="912" height="688" />
+
+---
+## Como rodar
+- Faça um clone desse repositório e acesse a pasta. 
+- Dentro da pasta basta executar o arquivo app.py usando Python 3
+  ```python3 app.py```
--- app.py
+++ app.py
@@ -26,57 +26,59 @@ Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser 
 
 # 1 continua e zero para
 continuar = "1"
-
-while True:
-    if continuar != "1":
-        break
-    baralho = cria_baralho()
+try:
     while True:
-        if len(baralho) == 0:
-            continuar = input("Você GANHOU! Deseja jogar novamente? 1 para sim e 0 para não [1/0]")
+        if continuar != "1":
             break
+        baralho = cria_baralho()
+        while True:
+            if len(baralho) == 0:
+                continuar = input("Você GANHOU! Deseja jogar novamente? 1 para sim e 0 para não [1/0]")
+                break
 
-        indice = 1
-        for carta in baralho:
-            print(f"\033[34m {indice}. {adiciona_cor(carta)} ")
+            indice = 1
+            for carta in baralho:
+                print(f"\033[34m {indice}. {adiciona_cor(carta)} ")
+                
+                indice += 1
             
-            indice += 1
-        
-        if possui_movimentos_possiveis(baralho):
-            movimento = int(input("Escolha uma carta para movimentar: "))
+            if possui_movimentos_possiveis(baralho):
+                movimento = int(input("Escolha uma carta para movimentar: "))
 
-            possibilidades = lista_movimentos_possiveis(baralho, movimento-1)
-            if len(possibilidades) == 0:
-                print("Essa carta não pode ser movida, tente novamente!")
-            else:
-                if len(possibilidades) > 1:
-                    print(f"Essas são as opções para movimento da carta {baralho[movimento-1]}")
-                    indice_possibilidades = 1
-                    for p in possibilidades:
-                        print(f"{indice_possibilidades}. {baralho[movimento-1-p]}")
-                        indice_possibilidades += 1
-                    while True:
-                        escolha_movimento = int(input("Escolha uma opção: ").strip())
-                        if escolha_movimento > len(possibilidades) and escolha_movimento < 1:
-                            print("Escolha inválida, tente novamente!")
-                            indice_possibilidades = 1
-                            for p in possibilidades:
-                                print(f"{indice_possibilidades}. {baralho[movimento-p]}")
-                                indice_possibilidades += 1
-                        else:
-                            print(f"Empilhar a carta {baralho[movimento-1]} sobre a carta {baralho[movimento-1-possibilidades[escolha_movimento-1]]}")
-                            empilha(baralho, movimento-1, movimento-1-possibilidades[escolha_movimento-1])
-                        
-                            break
-                elif len(possibilidades) == 0:
-                    print("Nenhum movimento possível, tente novamente!")
+                possibilidades = lista_movimentos_possiveis(baralho, movimento-1)
+                if len(possibilidades) == 0:
+                    print("Essa carta não pode ser movida, tente novamente!")
                 else:
-                
-                    empilha(baralho, movimento-1, movimento-1-possibilidades[0])
-        else:
-            continuar = input("Você PERDEU! Deseja jogar novamente? 1 para sim e 0 para não [1/0]")
-            break
-                
+                    if len(possibilidades) > 1:
+                        print(f"Essas são as opções para movimento da carta {baralho[movimento-1]}")
+                        indice_possibilidades = 1
+                        for p in possibilidades:
+                            print(f"{indice_possibilidades}. {baralho[movimento-1-p]}")
+                            indice_possibilidades += 1
+                        while True:
+                            escolha_movimento = int(input("Escolha uma opção: ").strip())
+                            if escolha_movimento > len(possibilidades) and escolha_movimento < 1:
+                                print("Escolha inválida, tente novamente!")
+                                indice_possibilidades = 1
+                                for p in possibilidades:
+                                    print(f"{indice_possibilidades}. {baralho[movimento-p]}")
+                                    indice_possibilidades += 1
+                            else:
+                                print(f"Empilhar a carta {baralho[movimento-1]} sobre a carta {baralho[movimento-1-possibilidades[escolha_movimento-1]]}")
+                                empilha(baralho, movimento-1, movimento-1-possibilidades[escolha_movimento-1])
+                            
+                                break
+                    elif len(possibilidades) == 0:
+                        print("Nenhum movimento possível, tente novamente!")
+                    else:
+                    
+                        empilha(baralho, movimento-1, movimento-1-possibilidades[0])
+            else:
+                continuar = input("Você PERDEU! Deseja jogar novamente? 1 para sim e 0 para não [1/0]")
+                break
+except KeyboardInterrupt:
+    print("\n")
+    print("Você encerrou o jogo!")
                 
         
             
